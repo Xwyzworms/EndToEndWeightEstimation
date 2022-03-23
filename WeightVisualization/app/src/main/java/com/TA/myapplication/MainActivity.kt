b67590d0077411db5820392a4516546eb9c8d614
XwyzWorm <pratamaazmi1@gmail.com>
2022-03-23 20:59:20 +0700

    done the design

--- WeightVisualization/app/src/main/java/com/TA/myapplication/MainActivity.kt
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.TA.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.TA.myapplication.databinding.ActivityMainBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+
 }
---
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/repository/DataRepository.kt
@@ -0,0 +1,4 @@
+package com.TA.myapplication.repository
+
+class DataRepository {
+}
