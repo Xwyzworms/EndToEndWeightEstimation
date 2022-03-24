73043e49245983d07fb46afed284ec77c8d0f425
XwyzWorm <pratamaazmi1@gmail.com>
2022-03-24 14:25:57 +0700

    Configure all Things ( Dummy ) for end to end Application, Connected with Firebase

--- WeightVisualization/app/src/main/java/com/TA/myapplication/HomeFragment.kt
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/HomeFragment.kt
@@ -1,19 +1,52 @@
 package com.TA.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.TA.myapplication.databinding.FragmentHomeBinding
+import com.TA.myapplication.ui.HomeFragmentViewModel
+import com.TA.myapplication.utility.ViewModelFactory
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var factory : ViewModelFactory
+    private lateinit var homeViewModel  : HomeFragmentViewModel
+
+    private var dataPath : String = "data"
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        factory = ViewModelFactory.getInstance()
+        homeViewModel = ViewModelProvider(this, factory)[HomeFragmentViewModel::class.java]
+        val dataJson : DatabaseReference? = homeViewModel.getTheJson(dataPath)
+        Log.d(TAG, dataJson.toString())
+        if (dataJson != null) {
+            dataJson.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    Log.d(TAG, snapshot.value.toString())
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d(TAG,"Error Here ${error.message.toString()}")
+                }
+
+            })
+        }
+        else {
+            Log.d(TAG, "NO DATA !!")
+        }
     }
 
     override fun onCreateView(
@@ -22,6 +55,10 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    companion object {
+        private val TAG :String = HomeFragment::class.java.simpleName
     }
 
 
--- WeightVisualization/app/src/main/java/com/TA/myapplication/utility/ViewModelFactory.kt
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/utility/ViewModelFactory.kt
@@ -1,6 +1,35 @@
 package com.TA.myapplication.utility
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.TA.myapplication.repository.DataRepository
+import com.TA.myapplication.ui.HomeFragmentViewModel
+import com.google.firebase.database.DatabaseReference
 
-class ViewModelFactory : ViewModelProvider {
+class ViewModelFactory(private  val dataRepository: DataRepository) : ViewModelProvider.NewInstanceFactory(){
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        when {
+            modelClass.isAssignableFrom(HomeFragmentViewModel::class.java) -> {
+                return HomeFragmentViewModel(dataRepository) as T
+            }
+        }
+        throw IllegalArgumentException("Incorrect Class ${modelClass.simpleName}")
+    }
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE : ViewModelFactory ? = null
+
+        @JvmStatic
+        fun getInstance() : ViewModelFactory{
+            return INSTANCE ?: synchronized(this) {
+                val instance = ViewModelFactory(Injection.provideRepository())
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
 }
