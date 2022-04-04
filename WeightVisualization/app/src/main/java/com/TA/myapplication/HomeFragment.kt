f5f874f51753d52718e4eba010cc69b984a76e46
XwyzWorm <pratamaazmi1@gmail.com>
2022-04-04 16:04:12 +0700

    add Dummy implementation ( works )

--- WeightVisualization/app/src/main/java/com/TA/myapplication/HomeFragment.kt
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.TA.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,14 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.TA.myapplication.data.Truck
 import com.TA.myapplication.databinding.FragmentHomeBinding
 import com.TA.myapplication.ui.HomeFragmentViewModel
+import com.TA.myapplication.utility.AxisDateFormatter
 import com.TA.myapplication.utility.ViewModelFactory
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
-
+import com.github.mikephil.charting.data.LineData
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
@@ -25,12 +31,14 @@ class HomeFragment : Fragment() {
 
     private var dataPath : String = "data"
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         factory = ViewModelFactory.getInstance()
         homeViewModel = ViewModelProvider(this, factory)[HomeFragmentViewModel::class.java]
         val dataJson : DatabaseReference? = homeViewModel.getTheJson(dataPath)
+
+        prepareChart(createDummyTrucks())
+
         Log.d(TAG, dataJson.toString())
         if (dataJson != null) {
             dataJson.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -55,6 +63,38 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    fun createDummyTrucks() : ArrayList<Truck>{
+        val trucks : ArrayList<Truck> = ArrayList()
+        trucks.add(Truck("a",20.0,340.0))
+        trucks.add(Truck("a",12.0,420.0))
+        trucks.add(Truck("a",12.0,140.0))
+        trucks.add(Truck("a",3.0,140.0))
+        trucks.add(Truck("a",8.0,410.0))
+        trucks.add(Truck("a",22.0,220.0))
+        trucks.add(Truck("a",15.0,340.0))
+        trucks.add(Truck("a",31.0,520.0))
+        trucks.add(Truck("a",2.0,430.0))
+        trucks.add(Truck("a",1.0,240.0))
+        trucks.add(Truck("a",5.0,240.0))
+        trucks.add(Truck("a",10.0,140.0))
+        return trucks
+    }
+
+    private fun prepareChart(seriesData : ArrayList<Truck> ) {
+        binding.linechart.setTouchEnabled(true)
+        binding.linechart.setPinchZoom(true)
+        val data = homeViewModel.prepareDummyData(seriesData)
+        val dates = homeViewModel.dummyDates()
+        val dataset : List<LineDataSet> = homeViewModel.makeDummyDataset(data)
+        binding.linechart.xAxis.valueFormatter = AxisDateFormatter(dates.toArray(arrayOfNulls<String>(dates.size)))
+        binding.linechart.data = LineData(dataset[0],dataset[1])
+        val legend = binding.linechart.legend
+        legend.isEnabled = true
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+
     }
 
     companion object {
--- WeightVisualization/app/src/main/java/com/TA/myapplication/ui/HomeFragmentViewModel.kt
+++ WeightVisualization/app/src/main/java/com/TA/myapplication/ui/HomeFragmentViewModel.kt
@@ -1,12 +1,79 @@
 package com.TA.myapplication.ui
 
+import android.graphics.Color
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.TA.myapplication.HomeFragment
+import com.TA.myapplication.data.Truck
 import com.TA.myapplication.repository.DataRepository
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.firebase.database.DatabaseReference
 
 class HomeFragmentViewModel(private val dataRepository : DataRepository) : ViewModel(){
 
     fun getTheJson(path : String ) : DatabaseReference?{
         return dataRepository.getJsonFromDatabase(path)
+    }
+
+    fun getTruckInformation(){
+    }
+
+    fun prepareDummyData(list_weights : List<Truck>) : List<ArrayList<Entry>> {
+        val currentTruckMin = ArrayList<Entry>()
+        val currentTruckMax = ArrayList<Entry>()
+
+        Log.d(TAG, list_weights.toString())
+        for (i in 0 .. list_weights.size - 1) {
+            currentTruckMin.add(Entry(i.toFloat(),list_weights[i].standard_weight.toFloat()))
+            currentTruckMax.add(Entry(i.toFloat(),list_weights[i].full_weight.toFloat()))
+        }
+        return listOf(currentTruckMin,currentTruckMax)
+    }
+
+
+     fun dummyDates() : ArrayList<String> {
+        val date = ArrayList<String>()
+        date.add("01-Apr")
+        date.add("02-Apr")
+        date.add("03-Apr")
+        date.add("04-Apr")
+        date.add("05-Apr")
+        date.add("06-Apr")
+        date.add("07-Apr")
+        date.add("08-Apr")
+        date.add("09-Apr")
+        date.add("10-Apr")
+
+        return date
+    }
+    private fun prepareDataset(dataset : LineDataSet,type : String ="min") : LineDataSet{
+        dataset.mode = LineDataSet.Mode.CUBIC_BEZIER
+        dataset.circleRadius = 5f
+        if (type == "min") {
+            dataset.color = Color.BLUE
+
+            dataset.setCircleColor(Color.BLUE)
+        }
+        else if (type=="max") {
+            dataset.color = Color.RED
+            dataset.setCircleColor(Color.RED)
+        }
+        return dataset
+    }
+
+    fun makeDummyDataset(listData : List<ArrayList<Entry>>) : List<LineDataSet> {
+        val minData = listData[0]
+        val maxData = listData[1]
+        var minDataset = LineDataSet(minData , "Minimum")
+        var maxDataset = LineDataSet(maxData , "Minimum")
+        minDataset = prepareDataset(minDataset,"min")
+        maxDataset = prepareDataset(maxDataset,"max")
+        return listOf(minDataset,maxDataset)
+    }
+
+
+    companion object {
+        val TAG : String = HomeFragment::class.java.simpleName
     }
 }
